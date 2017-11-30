780f3fb7bbdf3aebc73bdfedfbaf822253f00e9b
arieldsr <dsrariel@gmail.com>
2017-11-30 09:42:10 -0200

    Corrigindo erro no if do formulário

--- login_funcionarios.php
+++ login_funcionarios.php
@@ -1,6 +1,6 @@
 <?php
 	//Se houver um formulário disponível
-	if (REQUESTED_METHOD[SERVER] = 'POST'){
+	if ($_SERVER['REQUESTED_METHOD'] == 'POST'){
 		$id = $_POST['id']; //variável id recebe valor do campo 'id' do vetor $_POST
 		$senha = $_POST['senha'];
 	}
