63762869c530c7af457314f28b6f2dd855028ae5
arieldsr <dsrariel@gmail.com>
2017-11-30 09:24:14 -0200

    Adicionando arquivo php que trata das informações do formulário de login dos funcionários

---
+++ login_funcionarios.php
@@ -0,0 +1,7 @@
+<?php
+	//Se houver um formulário disponível
+	if (REQUESTED_METHOD[SERVER] = 'POST'){
+		$id = $_POST['id']; //variável id recebe valor do campo 'id' do vetor $_POST
+		$senha = $_POST['senha'];
+	}
+?>
